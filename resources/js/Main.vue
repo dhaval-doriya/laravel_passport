0eb9a4ee25856c76aaad5b4da140b9663ce2a0dc
dhaval.d <dhaval.d@webcodegenie.com>
2024-02-16 15:34:25 +0530

    temp route

--- resources/js/Main.vue
+++ resources/js/Main.vue
@@ -3,6 +3,7 @@
 </script>
  
 <template>
-    sdffds
+    sdffdssda
      <Counter />
+     
 </template>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,57 @@
-import './bootstrap';
+import "./bootstrap";
+import { createApp } from "vue";
+import Main from "./Main.vue";
 
-import { createApp } from 'vue'
-import Main from './Main.vue'
+// const router = new VueRouter({
+//     mode: "history", 
+//     // base: process.env.BASE_URL,
+//     routes,
+// });
 
+// Vue.config.productionTip = false;
 
-const app = createApp()
+// Vue.use(VueRouter);
 
-app.component('Mainapp', Main )
+// app = new Vue({
+//     router, // Use the router configuration
+//     render: (h) => h(App),
+// }).$mount("#app");
 
-app.mount('#app')
+const app = createApp();
+app.component("Mainapp", Main);
+app.mount("#app");
 
+// import {VueRouter} from 'vue-router';
 
+// Vue.use(Router);
 
+// Vue.config.productionTip = false;
+
+// new Vue({
+//   router, // Use the router configuration
+//   render: h => h(App),
+// }).$mount('#app');
+
+// const app = createApp();
+
+// app.component("Mainapp", Main);
+
+// app.mount("#app");
+
+// export default new VueRouter({
+//     routes: [
+//         {
+//             path: "/login",
+//             component: Login,
+//         },
+//         {
+//             path: "/register",
+//             component: Register,
+//         },
+//         // {
+//         //     path: "/dashboard",
+//         //     component: Dashboard,
+//         //     beforeEnter: requireAuth,
+//         // },
+//     ],
+// });
